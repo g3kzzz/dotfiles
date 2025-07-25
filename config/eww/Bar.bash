3231b3d6d322833ad7d0ed06a13f96a763e04f15
IamGenarov <fabian.huamani@estudiante.ucsm.edu.pe>
2025-07-25 09:00:05 -0500

    COnfiguracion de carpeta NVIM

--- config/eww/Bar.bash
+++ config/eww/Bar.bash
@@ -1,15 +1,30 @@
-# This file launch the bar/s
+#!/bin/sh
+# ===============================================
+# Este archivo lanza la(s) barra(s)
+#       ⠀⠀⢀⣴⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#       ⠀⢠⣿⣿⢿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#       ⢀⡾⠋⠀⣰⣿⣿⠻⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#       ⠘⠀⠀⢠⣿⣿⠃⠀⠈⠻⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#       ⠀⠀⠀⢸⣿⡇⠀⠀⠀⣼⣉⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀
+#       ⠀⠀⠀⢹⣿⡇⠀⠀⠀⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠟⣿⠉⠁
+#       ⠀⠀⠀⠸⣿⣿⣄⠀⠀⠘⢿⣿⡵⠋⠙⢿⣦⡀⠀⠀⣤⣠⠀⣠⣿⡅⠀⣿⠀⠀
+#       ⠀⠀⠀⠀⠈⠻⢿⣿⣶⣤⣄⣀⠀⠀⠀⠈⠻⣷⣄⣠⣿⣿⡼⠋⠛⣡⡼⠋⠀⠀
+#       ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠻⠿⠿⠷⠶⠶⠾⣿⡿⠋⠻⣟⠉⠁⠀⠀⠀⠀⠀
+#       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# Author: g3k
+# Rep: https://github.com/IamGenarov/blackbspwm
+# ===============================================
 
-# Function for generating workspaces.yuck file with eww widgets
+# Función para generar el archivo workspaces.yuck con los widgets de eww
 generate_eww_workspaces() {
-    eww_file="${HOME}/.config/bspwm/rices/${RICE}/bar/workspaces.yuck"
+    eww_file="${HOME}/.config/bspwm/eww/bar/workspaces.yuck"
     monitors=$(bspc query -M --names)
     count=0
     listen_workspaces=""
     widgets=""
-    workspace_widgets=";; Workspaces Widgets ;;\n"
+    workspace_widgets=";; Widgets de escritorios ;;\n"
 
-    printf "%s\n" ";; Workspaces ;;" > "$eww_file"
+    printf "%s\n" ";; Escritorios ;;" > "$eww_file"
 
     for m in $monitors; do
         workspace_name="workspace${count}"
@@ -20,25 +35,26 @@ generate_eww_workspaces() {
     done
     printf "%b" "$listen_workspaces" >> "$eww_file"
     printf "%b" "$workspace_widgets" >> "$eww_file"
-    printf "%b" ";; Workspaces Main Widget ;;\n(defwidget workspaces [monitor]\n   (box    :orientation \"v\"\n           :space-evenly \"false\"\n           :valign \"start\"\n$widgets))" >> "$eww_file"
+    printf "%b" ";; Widget principal de escritorios ;;\n(defwidget workspaces [monitor]\n   (box    :orientation \"v\"\n           :space-evenly \"false\"\n           :valign \"start\"\n$widgets))" >> "$eww_file"
 }
 
 generate_eww_workspaces
 
-# Get a list of monitors and sort them so that the primary monitor is first
+# Obtener la lista de monitores y ordenarlos para que el monitor primario esté primero
 monitors=$(xrandr -q | grep -w 'connected' | sort -k3n | cut -d' ' -f1)
 count=0
 for m in $monitors; do
-    eww -c "${HOME}/.config/bspwm/rices/${RICE}/bar" open bar --id "$m" --arg monitor="$m" --toggle --screen "$count"
+    eww -c "${HOME}/.config/bspwm/eww/bar" open bar --id "$m" --arg monitor="$m" --toggle --screen "$count"
     count=$((count + 1))
 done
 
-# Fix eww when entering fullscreen state
-	bspc subscribe node_state | while read -r _ _ _ _ state flag; do
-		[ "$state" = "fullscreen" ] || continue
-			if [ "$flag" = "on" ]; then
-				HideBar -h
-			else
-				HideBar -u
-			fi
-	done &
+# Corregir el comportamiento de eww al entrar en modo pantalla completa
+bspc subscribe node_state | while read -r _ _ _ _ state flag; do
+    [ "$state" = "fullscreen" ] || continue
+    if [ "$flag" = "on" ]; then
+        HideBar -h
+    else
+        HideBar -u
+    fi
+done &
+
--- config/eww/theme-config.bash
+++ config/eww/theme-config.bash
@@ -1,10 +1,10 @@
 #############################
-#		z0mbi3 Theme		#
+#		Tema z0mbi3		#
 #############################
 # Copyright (C) 2021-2025 gh0stzk <z0mbi3.zk@protonmail.com>
 # https://github.com/gh0stzk/dotfiles
 
-# (Decay, decayce variant) colorcheme
+# Esquema de colores (Decay, variante decayce)
 bg="#0d0f18"
 fg="#a5b6cf"
 
@@ -28,51 +28,27 @@ whiteb="#cbced3"
 accent_color="#151720"
 arch_icon="#0f94d2"
 
-# Bspwm options
-BORDER_WIDTH="0"		# Bspwm border
-TOP_PADDING="1"
+# Opciones de Bspwm
+BORDER_WIDTH="0"		# Borde de ventanas
+TOP_PADDING="2"
 BOTTOM_PADDING="1"
 LEFT_PADDING="60"
 RIGHT_PADDING="1"
-NORMAL_BC="#3d414f"		# Normal border color
-FOCUSED_BC="#93cee9"	# Focused border color
+NORMAL_BC="#3d414f"		# Color del borde normal
+FOCUSED_BC="#93cee9"	# Color del borde enfocado
 
-# Terminal font & size
+# Fuente y tamaño de la terminal
 term_font_size="16"
 term_font_name="JetBrainsMono Nerd Font"
 
-# Picom options
-P_FADE="true"			# Fade true|false
-P_SHADOWS="true"		# Shadows true|false
-SHADOW_C="#000000"		# Shadow color
-P_CORNER_R="6"			# Corner radius (0 = disabled)
-P_BLUR="false"			# Blur true|false
-P_ANIMATIONS="@"		# (@ = enable) (# = disable)
-P_TERM_OPACITY="1.0"	# Terminal transparency. Range: 0.1 - 1.0 (1.0 = disabled)
+#Colores de Jgmenu
+#jg_bg="$bg"			# Fondo
+#jg_fg="$fg"			# Texto
+#jg_sel_bg="$accent_color"	# Fondo de selección
+#jg_sel_fg="$fg"		# Texto seleccionado
+#jg_sep="$black"		# Separadores
 
-# Dunst
-dunst_offset='(0, 20)'
-dunst_origin='top-center'
-dunst_transparency='0'
-dunst_corner_radius='6'
-dunst_font='JetBrainsMono NF Medium 9'
-dunst_border='0'
-dunst_frame_color="$cyan"
-dunst_icon_theme="Catppuccin-Mocha"
-# Dunst animations
-dunst_close_preset="fly-out"
-dunst_close_direction="up"
-dunst_open_preset="fly-in"
-dunst_open_direction="up"
-
-# Jgmenu colors
-jg_bg="$bg"
-jg_fg="$fg"
-jg_sel_bg="$accent_color"
-jg_sel_fg="$fg"
-jg_sep="$black"
-
-# Rofi menu font and colors
+# Fuente y colores del menú Rofi
 rofi_font="JetBrainsMono NF Bold 9"
 rofi_background="$bg"
 rofi_bg_alt="$accent_color"
@@ -82,28 +58,17 @@ rofi_selected="$magenta"
 rofi_active="$green"
 rofi_urgent="$red"
 
-# Screenlocker
-sl_bg="${bg}"
-sl_fg="${fg}"
-sl_ring="${bg}"
-sl_wrong="${red}"
-sl_date="${fg}"
-sl_verify="${green}"
+# Pantalla de bloqueo (screenlocker)
+sl_bg="${bg}"			# Fondo
+sl_fg="${fg}"			# Texto
+sl_ring="${bg}"			# Anillo
+sl_wrong="${red}"		# Color para contraseña incorrecta
+sl_date="${fg}"			# Color de fecha
+sl_verify="${green}"	# Color de verificación
 
-# Gtk theme
+# Tema GTK
 gtk_theme="Decayce-zk"
 gtk_icons="Luv-Folders"
 gtk_cursor="Qogirr-Dark"
 geany_theme="z0mbi3-Decay"
 
-# Wallpaper engine
-# Available engines:
-# - Theme	(Set a random wallpaper from rice directory)
-# - CustomDir	(Set a random wallpaper from the directory you specified)
-# - CustomImage	(Sets a specific image as wallpaper)
-# - CustomAnimated (Set an animated wallpaper. "mp4, mkv, gif")
-# - Slideshow (Change randomly every 15 minutes your wallpaper from Walls rice directory)
-ENGINE="Theme"
-CUSTOM_DIR="/path/to/dir"
-CUSTOM_WALL="/path/to/image"
-CUSTOM_ANIMATED="$HOME/.config/bspwm/src/assets/animated_wall.mp4"
